cefb4d97c9b05a6017d3140d5f2d0c3422eca42e
s0288 <alex.gansmann@gmail.com>
2019-06-04 18:31:21 +0200

    wip typeform

--- strowan_bot/classes/typeform_bot.py
+++ strowan_bot/classes/typeform_bot.py
@@ -21,32 +21,32 @@ r = requests.get("https://api.typeform.com/forms/ASHAdA/responses?since={}T00%3A
 r.encoding = 'utf-8'
 json_data = json.loads(r.text)
 
-try:
-    for i in range(0,len(json_data['items'])):
-        custom_id = json_data['items'][i]["hidden"]["id"]
-        landing_id = json_data['items'][i]["landing_id"]
-        response_id = json_data['items'][i]["response_id"]
-        landed_at = json_data['items'][i]["landed_at"]
-        submitted_at = json_data['items'][i]["submitted_at"]
-        if json_data['items'][i].get("answers") != None:
-            for j in range(0,len(json_data['items'][i]["answers"])):
-                field_id = json_data['items'][i]["answers"][j]["field"]["id"]
-                field_type = json_data['items'][i]["answers"][j]["field"]["type"]
-                answer_type = json_data['items'][i]["answers"][j]["type"]
-                if answer_type == 'number':
-                    answer_value = json_data['items'][i]["answers"][j]["number"]
-                elif answer_type == 'choice':
-                    answer_value = json_data['items'][i]["answers"][j]["choice"]["label"]
-                elif answer_type == 'text':
-                    answer_value = json_data['items'][i]["answers"][j]["text"]
-                elif answer_type == 'text':
-                    answer_value = json_data['items'][i]["answers"][j]["text"]
-                if DBBot.check_typeform(landing_id, field_id) == 0:
-                    received_at = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-                    # add user_id, first_name, created_at, is_bot, language_code
-                    DBBot.add_typeform_response(custom_id, landing_id, response_id, landed_at, submitted_at, received_at, field_id, field_type, answer_type, answer_value)
-                    print("Added: Typeform response")
-                else:
-                    print("Typeform response already there")
-except Exception as e:
-    print(e)
+# try:
+for i in range(0,len(json_data['items'])):
+    custom_id = json_data['items'][i]["hidden"]["id"]
+    landing_id = json_data['items'][i]["landing_id"]
+    response_id = json_data['items'][i]["response_id"]
+    landed_at = json_data['items'][i]["landed_at"]
+    submitted_at = json_data['items'][i]["submitted_at"]
+    if json_data['items'][i].get("answers") != None:
+        for j in range(0,len(json_data['items'][i]["answers"])):
+            field_id = json_data['items'][i]["answers"][j]["field"]["id"]
+            field_type = json_data['items'][i]["answers"][j]["field"]["type"]
+            answer_type = json_data['items'][i]["answers"][j]["type"]
+            if answer_type == 'number':
+                answer_value = json_data['items'][i]["answers"][j]["number"]
+            elif answer_type == 'choice':
+                answer_value = json_data['items'][i]["answers"][j]["choice"]["label"]
+            elif answer_type == 'text':
+                answer_value = json_data['items'][i]["answers"][j]["text"]
+            elif answer_type == 'text':
+                answer_value = json_data['items'][i]["answers"][j]["text"]
+            if DBBot.check_typeform(landing_id, field_id) == 0:
+                received_at = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+                # add user_id, first_name, created_at, is_bot, language_code
+                DBBot.add_typeform_response(custom_id, landing_id, response_id, landed_at, submitted_at, received_at, field_id, field_type, answer_type, answer_value)
+                print("Added: Typeform response")
+            else:
+                print("Typeform response already there")
+# except Exception as e:
+#     print(e)
