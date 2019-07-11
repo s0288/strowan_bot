266c4b26b90b6252adfb1b74ae7ee5cb731f72ba
s0288 <alex.gansmann@gmail.com>
2019-07-11 20:33:43 +0200

    corrected save location in analytics.py

--- strowan_bot/classes/analytics.py
+++ strowan_bot/classes/analytics.py
@@ -16,7 +16,7 @@ current_date = datetime.datetime.now().strftime("%Y-%m-%d")
 # create ipynb
 try:
     print('creating analytics notebook')
-    pm.execute_notebook('../../../analytics.ipynb', '../../../nad_app/static/reports/analytics' + "_" + current_date +'.ipynb', parameters = dict())
+    pm.execute_notebook('../../../analytics.ipynb', '../../../nad_app/static/reports/analytics/analytics' + "_" + current_date +'.ipynb', parameters = dict())
 except Exception as e:
     print('error creating analytics notebook')
     print(e)
@@ -24,11 +24,11 @@ except Exception as e:
 try:
     print('converting jupyter notebook to html...')
     ## add /home/s0288/.virtualenvs/my-virtualenv/bin/ to specify directory of virtual env for subprocess - reverts to default python 2.7 otherwise!
-    nbconverttohtml_nocode ="/home/s0288/.virtualenvs/my-virtualenv/bin/jupyter nbconvert --ExecutePreprocessor.kernel_name=python3.6 --template=nbextensions --to=html ../../../nad_app/static/reports/analytics" + "_" + current_date + ""
+    nbconverttohtml_nocode ="/home/s0288/.virtualenvs/my-virtualenv/bin/jupyter nbconvert --ExecutePreprocessor.kernel_name=python3.6 --template=nbextensions --to=html ../../../nad_app/static/reports/analytics/analytics" + "_" + current_date + ""
     os.system(nbconverttohtml_nocode)
     # remove the original jupyter file to save space
     print('removing jupyter notebook...')
-    os.remove('../../../nad_app/static/reports/analytics' + "_" + current_date + '.ipynb' + "")
+    os.remove('../../../nad_app/static/reports/analytics/analytics' + "_" + current_date + '.ipynb' + "")
 except Exception as e:
     print('error creating html file')
     print(e)
