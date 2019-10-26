d3c4a7fc0d983fdd83c5896cee3f7a9314a9043b
s0288 <alex.gansmann@gmail.com>
2019-10-27 00:18:30 +0200

    added check to only send activity on Sunday

--- strowan_bot/Helper/send_mail.py
+++ strowan_bot/Helper/send_mail.py
@@ -5,15 +5,19 @@ import sys #required because files in parent folder
 sys.path.append('../')
 import config
 
-# get activity values
-periods = [1, 4, 12]
-activity_txt = track_activity(periods)
+import datetime
 
-# send email
-server = smtplib.SMTP_SSL('smtpout.EUROPE.secureserver.net', 465)
-server.login(config.ADMIN_MAIL, config.ADMIN_MAIL_PW)
-fromaddr = config.ADMIN_MAIL
-toaddr = config.TARGET_MAIL
-msg = (f"From: {fromaddr}\r\nTo: {toaddr}\r\n\r\n{activity_txt}")
-server.sendmail(fromaddr, toaddr, msg)
-server.quit() 
+# only send on Sunday
+if datetime.datetime.today().weekday() == 6:
+    # get activity values
+    periods = [1, 4, 12]
+    activity_txt = track_activity(periods)
+
+    # send email
+    server = smtplib.SMTP_SSL('smtpout.EUROPE.secureserver.net', 465)
+    server.login(config.ADMIN_MAIL, config.ADMIN_MAIL_PW)
+    fromaddr = config.ADMIN_MAIL
+    toaddr = config.TARGET_MAIL
+    msg = (f"From: {fromaddr}\r\nTo: {toaddr}\r\n\r\n{activity_txt}")
+    server.sendmail(fromaddr, toaddr, msg)
+    server.quit() 
