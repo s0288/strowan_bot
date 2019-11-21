e1cce0bd3a4accf6ef3c5f1dbf3b63c1c9dd0b8f
s0288 <alex.gansmann@gmail.com>
2019-11-21 07:05:24 +0100

    fixed bug in meal_pic

--- strowan_bot/Helper/meal_pic.py
+++ strowan_bot/Helper/meal_pic.py
@@ -101,7 +101,7 @@ for user in ids:
     for num in range(count_df):
         # get image urls and created at
         if df["key_value"][num] in ('user_photo', 'meal_confirmed_text', 'meal_form_time'):
-            imgs.append("{}/{}_meal_entry_{}_{}_{}-{}-{}".format(str(df['created_at'][num].isocalendar()[0]) + "-" + str(df['created_at'][num].isocalendar()[1]), df['platform_user_id'][num], df['key_value'][num], df['created_at'][num].date(), df['created_at'][num].hour, df['created_at'][num].minute, df['created_at'][num].second))
+            imgs.append("{}/{}_mahlzeit_{}_{}_{}-{}-{}".format(str(df['created_at'][num].isocalendar()[0]) + "-" + str(df['created_at'][num].isocalendar()[1]), df['platform_user_id'][num], df['key_value'][num], df['created_at'][num].date(), df['created_at'][num].hour, df['created_at'][num].minute, df['created_at'][num].second))
             created_at.append(df['created_at'][num])
 
             ## only check for paths and reasons, when the current input is an image
