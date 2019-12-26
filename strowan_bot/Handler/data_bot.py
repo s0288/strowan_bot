9e83e55637591e527d9b2ab142e700f6b5d1092a
s0288 <alex.gansmann@gmail.com>
2019-12-26 20:23:34 +0100

    wip trigger removal

--- strowan_bot/Handler/data_bot.py
+++ strowan_bot/Handler/data_bot.py
@@ -249,5 +249,5 @@ if __name__ == '__main__':
     DataBot.add_trigger_for_times('assessment_text')
     DataBot.add_trigger_for_fast('fast_duration_integer', lookback=1)
     DBBot.delete_triggers_by_inactive_users()
-    DataBot.remove_triggers('fasten_feedback')
-    DataBot.remove_triggers('fasten_end')
+    DataBot.remove_triggers('/fasten_feedback')
+    DataBot.remove_triggers('/fasten_end')
